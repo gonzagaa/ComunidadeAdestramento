b9a6c561aa67709f8f776b11a3a3308fe4b8ed9b
gonzagaa <iigust4vo@gmail.com>
2025-03-24 21:07:44 -0300

    update

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ window.addEventListener("scroll", function () {
       navButton.classList.remove("desactive");
   }
 });
+
+  window.addEventListener('DOMContentLoaded', function () {
+    const isMobile = window.innerWidth <= 768;
+
+    const iframeDesktop = document.querySelector('iframe.desktop');
+    const iframeMobile = document.querySelector('iframe.mobile');
+
+    // Remove o src de ambos para garantir que nenhum carregue antes da verificação
+    iframeDesktop.removeAttribute('src');
+    iframeMobile.removeAttribute('src');
+
+    if (isMobile) {
+      iframeMobile.src = "https://player-vz-fa2c182e-ab9.tv.pandavideo.com.br/embed/?v=f9ec0c8f-cdb6-4bb5-9f48-d45f4e8dec3b";
+    } else {
+      iframeDesktop.src = "https://player-vz-fa2c182e-ab9.tv.pandavideo.com.br/embed/?v=d361b71a-0b84-4ba9-b95f-76adb8670c52";
+    }
+  });
